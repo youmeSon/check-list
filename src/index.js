16946f1b129adec6ddd229ea07e4825503b3f633
Youme <rosydarling@naver.com>
2023-09-01 00:00:25 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Items from './components/Items';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Items />
   </React.StrictMode>
 );
 
